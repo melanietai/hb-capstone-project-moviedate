a01eacf7fd4137a75e13d7632d8420f1266b8cee
Melanie Tai <mt337@cornell.edu>
2022-04-13 22:46:38 -0700

    Complete database.py

---
+++ database.py
@@ -0,0 +1,11 @@
+"""Script to create database tables"""
+
+import os
+import model
+import server
+
+os.system("dropdb moviedate")
+os.system('createdb moviedate')
+
+model.connect_to_db(server.app)
+model.db.create_all()

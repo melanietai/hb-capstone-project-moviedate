df15e135b5ff3b6f6490a487bef5297a9ae7835d
Melanie Tai <mt337@cornell.edu>
2022-04-20 18:32:09 -0700

    Add buttons to add/remove movie

--- static/js/moviedate.js
+++ static/js/moviedate.js
@@ -18,22 +18,48 @@ document.querySelector('#movie-keyword').addEventListener('submit', evt => {
         const movieInfoContent = `
         <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="poster" width="100"/><br>
         Movie Title: ${movie.original_title}
-        <details>
+        <details id=${movie.id}>
           <summary>More details:</summary>
           <p>Popularity: ${movie.popularity}</p>
           <p>Release date: ${movie.release_date}</p>
           <p>Overview: ${movie.overview}</p>
         </details>
-        <button
+        <form class="add-movie">
+          <input type="hidden" class="movie-name" value="${movie.original_title}">
+          <button type="submit">Add movie to RSVP</button>
+        </form>
         <br><br>
         `;
         console.log(movieInfoContent)
+        // we should be able to do this without another query
       document.querySelector('#movie-results').insertAdjacentHTML('afterbegin', movieInfoContent);
       }
+      const moviesToAdd = document.querySelectorAll('.add-movie');
+        for (const movie of moviesToAdd) {
+          movie.addEventListener('submit', evt => {
+          evt.preventDefault();
+          
+          movieName = evt.target.querySelector('input').value;
+          console.log(evt.target);
+          document.querySelector('#movies-added').insertAdjacentHTML('beforeend', 
+          `<li id=${movieName}>${movieName}</li>
+          <button id="remove-buttons">Remove movie</button>
+          </form>
+          `);
+        })
+      }
+      const removeButtons = document.querySelectorAll('#remove-buttons');
+        for (const button of removeButtons) {
+          button.addEvent.Listener('click', evt => {
+            evt.preventDefault();
+            evt.target.remove();
+          });
+        }
     });
 });
 
-        // <ul class="movie-info">
-        //   <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="poster" width="100"/>
-        //   <a href="#" id="${movie.id}" data-json="${movie.id}">${movie.original_title} </a>
-        // </ul>
+
+
+
+
+

d00ff959a03cbc28f13c5d1f907b76f4a77a7063
Melanie Tai <mt337@cornell.edu>
2022-04-28 17:49:15 -0700

    Create EventAt react component

---
+++ static/js/EventAt.jsx
@@ -0,0 +1,22 @@
+const EventAt = (props) => {
+  // const { onTitleChange, onDateChange, onTimeChange } = props; 
+
+  return (
+    <div className="event-at-container">
+        <label>
+          <p>Event name</p>
+          <input onChange={props.onTitleChange} type="text" name="title" required />
+        </label>
+        <label>
+          <p>Select a date</p>
+          <input onChange={props.onDateChange} type="date" name="date" required />
+        </label>
+        <label>
+          <p>Select a time</p>
+          <input onChange={props.onTimeChange} type="time" name="time" required />
+        </label>
+    </div>
+  )
+}
+
+

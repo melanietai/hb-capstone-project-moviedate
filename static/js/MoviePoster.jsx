ae70c93c498b87cb8df4ed04a03c30a877b0b9c7
Melanie Tai <mt337@cornell.edu>
2022-04-30 10:15:37 -0700

    Create MoviePoster component

---
+++ static/js/MoviePoster.jsx
@@ -0,0 +1,29 @@
+const MoviePoster = (props) => {
+  const { token } = useToken();
+  const [movie, setMovie] = React.useState([]);
+
+  const apiId = props.apiId;
+
+  React.useEffect(() => {
+    if (apiId) {
+      fetch(`/api/movie/${apiId}`, {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json',
+        },
+      }).then(res => res.json()).then(data => {
+        setMovie(data);
+      });
+    }
+  }, [apiId]);
+
+
+  return(
+    <div>
+      <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="poster" width="100" />
+    </div>
+  );
+
+
+};

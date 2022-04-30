d6956d77c8e9550a5411f350b51b9278cd52fd45
Melanie Tai <mt337@cornell.edu>
2022-04-30 10:14:52 -0700

    Create EventDetails component

---
+++ static/js/EventDetails.jsx
@@ -0,0 +1,12 @@
+const EventDetails = (props) => {
+
+  const event = props.event;
+
+
+  return(
+    <div>
+      <p>Event name: {event.title}</p>
+      <p>Event date and time: {event.event_at}</p>
+    </div>
+  );
+};

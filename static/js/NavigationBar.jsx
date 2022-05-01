2dd58e77a000af81fa0cfc3ed7fb8c988112bc67
Melanie Tai <mt337@cornell.edu>
2022-04-30 23:13:53 -0700

    Explore Bootstrap

--- static/js/NavigationBar.jsx
+++ static/js/NavigationBar.jsx
@@ -1,16 +1,16 @@
 const { Nav, Navbar, Container } = ReactBootstrap;
 
 const NavigationBar = (props) => {
-  
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="info" variant="info" expand="lg">
       <Container>
         <Navbar.Brand href="#home">Movie Date</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as="span"><Link to="/">Create event</Link></Nav.Link>
-            <Nav.Link as="span"><Link to="/events">Events</Link></Nav.Link>
+            <Nav.Link as="span"><Link to="/" style={{ textDecoration: 'none', color: '#FFF'}}>Create event</Link></Nav.Link>
+            <Nav.Link as="span"><Link to="/events" style={{ textDecoration: 'none', color: '#FFF'}}>Events</Link></Nav.Link>
             <Nav.Link to="#" onClick={props.onLogoutClick}>Logout</Nav.Link>
           </Nav>
         </Navbar.Collapse>

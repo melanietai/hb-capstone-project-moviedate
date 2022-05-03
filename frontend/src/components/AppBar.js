5b5cd0193d6fea76d0d8150d28faf7fbef56d767
Melanie Tai <mt337@cornell.edu>
2022-05-03 13:44:07 -0700

    Add AppBar component as navbar

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -1,9 +1,14 @@
-
+import { Link } from 'react-router-dom';
 
 const AppBar = (props) => {
 
-  return(
-    <div>Navigation Bar</div>
+  return (
+    <div>
+      Movie Date
+      <Link to="/">Create event</Link>
+      <Link to="/events">Events</Link>
+      <Link to="#" onClick={props.onLogoutClick}>Logout</Link>
+    </div>
   );
 };
 

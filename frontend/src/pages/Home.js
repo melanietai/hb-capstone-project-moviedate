c780c208ffa84271032dd8ae372087b14af60d75
Melanie Tai <mt337@cornell.edu>
2022-05-03 18:28:10 -0700

    Add styles to homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Login from '../components/Login';
 import Signup from '../components/Signup';
+import { Box, Container, Image, Stack, Heading, Flex, Spacer } from '@chakra-ui/react';
+import '../index.css'
 
 
 const Home = (props) => {
@@ -35,16 +37,64 @@ const Home = (props) => {
 
 
   return (
-    <div>
-      <div>
-        {children}
-      </div>
-      <div>
+    <>
+      <Box d="flex" align-items="center">
         {movies.map(movie => {
-        return <img key={movie.id} src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="poster" width="100" />
+        return (
+          <Stack>
+            <Image
+              boxSize='150px'
+              objectFit='cover'
+              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              alt='popular movie poster'
+            />
+          </Stack>
+        );
         })}
-      </div>
-    </div>
+      </Box>
+      <Box>
+        <Container maxWidth="container.xl">
+          <Flex midWidth='max-content' alignItems='center'>
+            <Box d="flex" align-items="center" py="20" flexDirection="row">
+              <Box mx="6">
+                <Heading as="h1" size="lg" color="yellow">
+                  <Box fontWeight="black">
+                    Welcome to The Movie Date!
+                  </Box>
+                </Heading>
+                <Heading as="h4" fontSize="lg" color="white">
+                  <Box m="8" fontWeight="medium">
+                  Want to watch a movie with friends at the convenience in your home? You are
+                  at the right place! 
+                  </Box>
+                  <Box m="8" fontWeight="medium">
+                  You can search for movies, create events, invite your 
+                  friends, view events, vote for movies to watch together on a virtual movie
+                  date, and many more!
+                  </Box>
+                </Heading>
+              </Box>
+            </Box>
+            <Spacer />
+              {children}
+          </Flex>
+        </Container>
+      </Box>
+      <Box d="flex" align-items="center" position="fixed" bottom="0">
+        {movies.map(movie => {
+        return (
+          <Stack>
+            <Image
+              boxSize='150px'
+              objectFit='cover'
+              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              alt='popular movie poster'
+            />
+          </Stack>
+        );
+        })}
+      </Box>
+    </>
   )
 };
 

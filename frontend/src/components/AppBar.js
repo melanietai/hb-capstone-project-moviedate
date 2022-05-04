b59419ee9ef2214a0dcb4bb774fcf3d2c2f2613d
Melanie Tai <mt337@cornell.edu>
2022-05-03 23:06:27 -0700

    Fix bug to navigate back to homepage after logout

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const AppBar = (props) => {
+  const navigate = useNavigate();
 
+  const onLogoutClick = (evt) => {
+    evt.preventDefault();
+    props.removeToken();
+    navigate("/");
+  };
   return (
     <div>
       Movie Date
       <Link to="/">Create event</Link>
       <Link to="/events">Events</Link>
-      <Link to="#" onClick={props.onLogoutClick}>Logout</Link>
+      <Link to="#" onClick={onLogoutClick}>Logout</Link>
     </div>
   );
 };

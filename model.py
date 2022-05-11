9211fa78bc2d501baa07a1ce437a7032b1b6733b
Melanie Tai <mt337@cornell.edu>
2022-05-10 23:02:31 -0700

    Replace postgres with postgresql in DATABASE_URL since sqlalchemy doesn't like it

--- model.py
+++ model.py
@@ -110,6 +110,8 @@ class Participant(db.Model):
 
 
 def connect_to_db(flask_app, db_uri=os.environ['DATABASE_URL'], echo=True):
+    if db_uri.startswith("postgres://"):     
+        db_uri = db_uri.replace("postgres://", "postgresql://", 1)
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False

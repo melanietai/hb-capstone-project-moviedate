113c2f72dce8dc574c86d99012d5053a075a51a3
Melanie Tai <mt337@cornell.edu>
2022-05-22 16:24:45 -0700

    Add more tests

--- test_server.py
+++ test_server.py
@@ -1,8 +1,8 @@
 from server import app
 from unittest import TestCase
 import unittest
-from model import connect_to_db, db, User
-from flask_jwt_extended import create_access_token
+from model import connect_to_db, db, User, Event, Movie, Participant
+from datetime import datetime
 
 
 class MyAppIntegrationTestCase(TestCase):
@@ -38,9 +38,9 @@ class MyAppIntegrationTestCase(TestCase):
         result = self.client.post(
             "/api/token", json={"email": "rsmith@example.com", "password": "rachel"}
         )
+        token = result.get_json().get("access_token")
 
         self.assertEqual(200, result.status_code)
-        token = result.get_json().get("access_token")
         self.assertEqual(str, type(token))
         self.assertNotEqual(0, len(token))
 
@@ -56,11 +56,110 @@ class MyAppIntegrationTestCase(TestCase):
                 "password": "rachel",
             },
         )
+        token = result.get_json().get("access_token")
 
         self.assertEqual(200, result.status_code)
-        token = result.get_json().get("access_token")
         self.assertEqual(str, type(token))
         self.assertNotEqual(0, len(token))
+
+    def test_show_event(self):
+        """Test show an event."""
+
+        # Add sample data
+        event = Event(
+            event_id=1, title="testing", event_at=datetime.now(), key="test"
+        )
+
+        db.session.add(event)
+        db.session.commit()
+
+        result = self.client.get(f"/api/events/{event.key}")
+        data = result.get_json() 
+
+        self.assertEqual(200, result.status_code)
+        self.assertEqual(event.event_id, data['event']['event_id'])
+        self.assertEqual(event.title, data['event']['title'])
+        self.assertEqual(event.key, data['event']['key'])
+        self.assertEqual(event.event_at.strftime('%a, %d %b %Y %H:%M:%S GMT'), data['event']['event_at'])
+
+    def test_show_movies(self):
+        """Test show all movies from an event."""
+
+        # Add sample data
+        event = Event(
+            event_id=1, title="testing", event_at=datetime.now(), key="test"
+        )
+
+        movie = Movie(
+            movie_id=123, api_id=321, vote_count=0, event_id=1
+        )
+
+        db.session.add_all([event, movie])
+        db.session.commit()
+
+        result = self.client.get(f"/api/movies/{event.event_id}")
+        data = result.get_json()
+
+        self.assertEqual(200, result.status_code)
+        self.assertEqual(movie.movie_id, data[0]['movie_id'])
+        self.assertEqual(movie.api_id, data[0]['api_id'])
+        self.assertEqual(movie.vote_count, data[0]['vote_count'])
+        self.assertEqual(movie.event_id, data[0]['event_id'])
+
+    def test_update_rsvp(self):
+        """Test update rsvp response."""
+
+        # Add sample data
+        event = Event(
+            event_id=1, title="testing", event_at=datetime.now(), key="test"
+        )
+        participant = Participant(
+            participant_id=1, email="abc@example.com", is_host=False, RSVP=False, voted=False, event_id=1
+        )
+
+        db.session.add_all([event, participant])
+        db.session.commit()
+
+        result = self.client.put(
+            f"/api/events/{event.key}/rsvp",
+            json={
+                "rsvp": True,
+                "email": "abc@example.com",
+            },
+        )
+        data = result.get_json()
+
+        self.assertEqual(200, result.status_code)
+        self.assertEqual(True, data['RSVP'])
+
+    def test_update_vote(self):
+        """Test update vote count for a movie."""
+
+        # Add sample data
+        event = Event(
+            event_id=1, title="testing", event_at=datetime.now(), key="test"
+        )
+        movie = Movie(
+            movie_id=123, api_id=321, vote_count=0, event_id=1
+        )
+        participant = Participant(
+            participant_id=123, email="abc@example.com", is_host=False, RSVP=False, voted=False, event_id=1
+        )
+
+        db.session.add_all([event, movie, participant])
+        db.session.commit()
+
+        result = self.client.put(
+            f"/api/events/{event.key}/vote-update",
+            json={
+                "apiIdList": ["321"],
+                "participantId": "123",
+            },
+        )
+        data = result.get_json()
+
+        self.assertEqual(200, result.status_code)
+        self.assertEqual(1, data[0]["vote_count"])
 
 
 if __name__ == "__main__":
